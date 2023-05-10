c1014a7b3ab71560bb32ddf51696df0370f3345c
sceonn <yoou201@naver.com>
2023-05-10 22:33:08 +0900

    lint: project 전체

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthModule } from './health/health.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { HealthModule } from './health/health.module'
 
 @Module({
   imports: [HealthModule],
