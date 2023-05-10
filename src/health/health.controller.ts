20929bd31d7fad9dc7bfc39df1a75b40fd50d20c
sceonn <yoou201@naver.com>
2023-05-10 22:31:13 +0900

    feat: g healch get api

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  HealthCheck,
+} from '@nestjs/terminus'
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([])
+  }
+}
