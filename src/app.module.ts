8a33698f7b010c9c45ec2a2615c94dea9202c8d6
sceonn <yoou201@naver.com>
2023-05-12 20:21:43 +0900

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { HealthModule } from './health/health.module'
+import { ConfigModule } from '@nestjs/config'
+import * as process from 'process'
+import mysqlConfig from './config/mysql.config'
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      load: [mysqlConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/mysql.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config'
+
+export default registerAs('database', () => ({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+}))
