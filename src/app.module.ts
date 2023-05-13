77d96ea7a6eaedeb39d423455082c48b7cf2a5f6
sceonn <yoou201@naver.com>
2023-05-13 20:26:13 +0900

    rename: 테스트 파일 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { HealthModule } from './health/health.module'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import * as process from 'process'
@@ -22,7 +20,5 @@ import { TypeOrmModule } from '@nestjs/typeorm'
       },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
