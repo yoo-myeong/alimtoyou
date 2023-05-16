377465fdbe13031e3df4dade299e34c6c5af4cc5
sceonn <yoou201@naver.com>
2023-05-16 19:23:48 +0900

    test: authCodeCreator create code

---
+++ __tests__/src/module/auth/authCodeCreator.spec.ts
@@ -0,0 +1,12 @@
+import { AuthCodeCreator } from '../../../../src/module/auth/authCodeCreator'
+
+describe('AuthCodeCreator', () => {
+  it('전달한 길이만큼의 숫자형 문자열을 생성합니다.', () => {
+    const sut = new AuthCodeCreator()
+    const length = 6
+
+    const result = sut.createCode(length)
+
+    expect(result.length).toBe(length)
+  })
+})
