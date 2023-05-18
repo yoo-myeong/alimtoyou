07aaea1042c3f9a625bc638ac4e59c9293f77ace
sceonn <yoou201@naver.com>
2023-05-18 19:20:11 +0900

    rename: int unit test 분리

--- __tests__/integration/src/module/auth/auth.service.spec.ts
+++ __tests__/integration/src/module/auth/auth.service.spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { getMySQLTypeOrmTestModule } from '../../entity/getMySQLTypeOrmTestModule'
-import { AuthService } from '../../../../src/module/auth/auth.service'
-import { AuthDomain } from '../../../../src/module/auth/auth.domain'
-import { AuthCodeCreator } from '../../../../src/module/auth/authCodeCreator'
+import { getMySQLTypeOrmTestModule } from '../../../../getMySQLTypeOrmTestModule'
+import { AuthService } from '../../../../../src/module/auth/auth.service'
+import { AuthDomain } from '../../../../../src/module/auth/auth.domain'
+import { AuthCodeCreator } from '../../../../../src/module/auth/authCodeCreator'
 import { DataSource, Repository } from 'typeorm'
-import { UserEntity } from '../../../../src/entity/user.entity'
+import { UserEntity } from '../../../../../src/entity/user.entity'
 import { getRepositoryToken } from '@nestjs/typeorm'
-import { EntityModule } from '../../../../src/entity/entity.module'
+import { EntityModule } from '../../../../../src/entity/entity.module'
 
 describe('AuthService', () => {
   let userRepository: Repository<UserEntity>
