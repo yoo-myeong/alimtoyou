4a276eff6b1b096195069f9526f5a6e356c12193
sceonn <yoou201@naver.com>
2023-08-29 12:49:00 +0900

    refactor: monorepo app

--- apps/external-api/src/app.module.ts
+++ apps/external-api/src/app.module.ts
@@ -6,7 +6,6 @@ import { mysqlConfig, mysqlConfigKey } from './config/mysql.config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './module/auth/auth.module'
 import { RedisModule } from '@liaoliaots/nestjs-redis'
-import { askForProjectName } from '@nestjs/cli/lib/utils/project-utils'
 import { redisConfigKey } from './config/redis.config'
 
 @Module({
